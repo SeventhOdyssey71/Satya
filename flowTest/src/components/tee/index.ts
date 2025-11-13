536de2a9213604185b7c3000d2333c25f7f64c62
SeventhOdyssey71 <odigie2004@gmail.com>
2025-11-13 12:41:01 +0100

    Add TEE verification components and models

---
+++ flowTest/src/components/tee/index.ts
@@ -0,0 +1,4 @@
+export { TEECompute } from './TEECompute';
+export { TEEAttestationCard } from './TEEAttestationCard';
+export { ModelVerificationFlow } from './ModelVerificationFlow';
+export type { TEEAttestationData } from './TEEAttestationCard';
