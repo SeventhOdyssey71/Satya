b9c2f2b4494193fda968ef6b17229c86e9bacd72
SeventhOdyssey71 <odigie2004@gmail.com>
2025-11-17 03:24:30 +0100

    Update upload page background and container width

--- flowTest/src/app/upload/page.tsx
+++ flowTest/src/app/upload/page.tsx
@@ -9,10 +9,10 @@ export const dynamic = 'force-dynamic'
 
 export default function UploadPage() {
  return (
-  <div className="min-h-screen bg-white">
+  <div className="min-h-screen bg-gray-50">
    <Header />
    <main className="relative z-10 py-6">
-    <div className="container max-w-7xl mx-auto px-6">
+    <div className="container max-w-4xl mx-auto px-6">
      <UploadProvider>
       <ModelUploadWizard />
      </UploadProvider>
